7717d7775d26adc949efee96f20b9f7325f9c97f
Sam Chen <hyuchen322111@gmail.com>
2020-06-08 22:09:29 +0800

    change to export csv only

--- automateRenderNB.R
+++ automateRenderNB.R
@@ -1,45 +1,52 @@
 library(rmarkdown)
 library(magrittr)
 
+source("e:/rf/percOutsideBeam/ftpTest/rCode/outsideBeamList.R")
+
 landingZone <- "f:/FTP Data/percOutsideBeam/landingZone/"
 temp <- "E:/rf/percOutsideBeam/ftpTest/temp/"
 output <- "f:/FTP Data/percOutsideBeam/Output/"
-tempHtml <- "e:/rf/percOutsideBeam/ftpTest/rCode/PercOutSideBeam.nb.html"
 tempCSV <- "e:/rf/percOutsideBeam/ftpTest/temp/All.csv"
-notebook <- "e:/rf/percOutsideBeam/ftpTest/rCode/PercOutSideBeam.Rmd"
+filterSiteList <- "e:/rf/percOutsideBeam/ftpTest/temp/filterSiteList.csv"
+filterCellList <- "e:/rf/percOutsideBeam/ftpTest/temp/filterCellList.csv"
 
-fileList <- list.files(landingZone, full.names = T)
+fileList <- list.files(landingZone, full.names = T, pattern = ".csv")
 
 if (length(fileList > 0)) {
   for (tfile in fileList) {
-    #check if the uploaded data is in csv format. Only process csv data
-    if (substr(tfile, nchar(tfile)-2, nchar(tfile)) == "csv") {
-      #copy the uploaded csv to temp folder and rename it as "All.csv"
-      file.copy(tfile, paste0(temp, "All.csv"))
-      #remove the uploaded csv
-      file.remove(tfile)
-      #render r notebook to html file
-      Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
-      file <- try(rmarkdown::render(notebook))
-      #if the notebook cannot be rendered, and then create an error msg
-      if (class(file) == "try-error") {
-        writeLines(text = paste0(basename(tfile), " has incorrect file format"),
-                   paste0(landingZone, "Error_", basename(tfile) %>%
-                            tools::file_path_sans_ext(), ".txt"))
-      }else {
-        #if render success, thne rename the html with uploaded file name and move it to output folder
-        file.copy(tempHtml, paste0(output, 
-                                   basename(tfile) %>% 
-                                     tools::file_path_sans_ext(),
-                                   ".html"))
-        #remove the temp html file
-        file.remove(tempHtml)
-        file.remove(tempCSV)
-      }
-      #remove the csv from temp folder
+    #copy the uploaded csv to temp folder and rename it as "All.csv"
+    file.copy(tfile, paste0(temp, "All.csv"))
+    #remove the uploaded csv
+    file.remove(tfile)  
+    #file <- try(rmarkdown::render(notebook))
+    try(outsideBeamList(tempCSV))
+    #if the notebook cannot be rendered, and then create an error msg
+    if (class(file) == "try-error") {
+      writeLines(text = paste0(basename(tfile), " has incorrect file format"),
+                 paste0(landingZone, "Error_", basename(tfile) %>%
+                          tools::file_path_sans_ext(), ".txt"))
+    }else {
+      #if success, then rename the filterSiteList csv with uploaded file name and move it to output folder
+      file.copy(filterSiteList, paste0(output, 
+                                 basename(tfile) %>% 
+                                   tools::file_path_sans_ext(),
+                                 "_siteList.csv"))
+      #remove the filterSiteList from temp folder
+      file.remove(filterSiteList)
+      
+      #if success, then rename the filterCellList csv with uploaded file name and move it to output folder
+      file.copy(filterCellList, paste0(output, 
+                                       basename(tfile) %>% 
+                                         tools::file_path_sans_ext(),
+                                       "_CellList.csv"))
+      #remove the filterCellList from temp folder
+      file.remove(filterCellList)
       
       
     }
-    
+    #remove the csv from temp folder
+    file.remove(tempCSV)
+      
   }
+    
 }
